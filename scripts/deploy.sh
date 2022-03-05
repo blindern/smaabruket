709c558321440b17578d7b16af38faa463a51ce5
Henrik Steen <henrist@henrist.net>
2022-03-05 17:22:49 +0100

    Switch to GitHub actions

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,4 +6,6 @@ if [ ! -f public/build/bundle.js ]; then
   exit 1
 fi
 
+ssh-keyscan coreos-2.foreningenbs.no >> ~/.ssh/known_hosts
+
 rsync -avz --delete public/ root@coreos-2.foreningenbs.no:/data/web-1-www/aliases/smaabruket/public/
