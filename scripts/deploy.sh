52e9d356c3e24b5c7c2f6696b389f71b5c5b9071
Henrik Steen <henrist@henrist.net>
2022-07-09 11:23:07 +0200

    Fix deploy target

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,6 +6,6 @@ if [ ! -e build ]; then
   exit 1
 fi
 
-ssh-keyscan coreos-2.foreningenbs.no >> ~/.ssh/known_hosts
+ssh-keyscan fcos-3.nrec.foreningenbs.no >> ~/.ssh/known_hosts
 
-rsync -avz --delete build/ root@coreos-2.foreningenbs.no:/data/web-1-www/aliases/smaabruket/public/
+rsync -avz --delete build/ root@fcos-3.nrec.foreningenbs.no:/var/mnt/data/web-1-www/aliases/smaabruket/public/
